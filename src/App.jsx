f82c93a7da3de75a93a8b9cd40765fb019db0c74
MICHAL FERNANDEZ <michaeltkd14@gmail.com>
2024-06-20 16:53:47 -0600

    Add  guitars

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './componentes/Header' 
+import Guitar from './componentes/guitar'
+import { db } from './data/db'
+function App() {
+  const [data,setData]=useState(db)
+  const [cart,setCart]=useState([])
+
+function addToCart(item){
+
+  const itemExists = cart.findIndex((guitar) => guitar.id===item.id)
+  console.log(itemExists)
+  setCart(prevCart=>[...prevCart, item])
+
+}
+
+
+  return (
+    <>
+     <Header/>
+    
+
+    <main className="container-xl mt-5">
+        <h2  className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+            {data.map((guitar)=>(
+                <Guitar
+                key={guitar.id}
+                  guitar={guitar}
+                  cart={cart}
+                  setCart={setCart}
+                  addToCart={addToCart}
+                /> 
+            ))}
+        </div>
+    </main>
+
+
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+    </footer>
+
+    </>
+  )
+}
+
+export default App
